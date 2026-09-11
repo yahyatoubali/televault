1c1254f7a03aaf7affdf35c86eab75970a8db1a5
Yahya Toubali <yahya@yahyatoubali.me>
2026-09-11 23:51:39 +0100

    fix(brew): fill real v4.0.2 asset hashes in Formula

--- Formula/televault.rb
+++ Formula/televault.rb
@@ -10,22 +10,22 @@ class Televault < Formula
   on_macos do
     on_arm do
       url "https://github.com/yahyatoubali/televault/releases/download/v#{version}/televault-v#{version}-darwin-arm64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_ARM64_SHA256_AFTER_RELEASE"
+      sha256 "d7697e7fb3aebba0401802114a8fa1c6248fe7cec356935d320a7592ff9ba130"
     end
     on_intel do
       url "https://github.com/yahyatoubali/televault/releases/download/v#{version}/televault-v#{version}-darwin-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_X86_64_SHA256_AFTER_RELEASE"
+      sha256 "4ae73ab42a09d99e9efc017c9dbef2d394774f3de84c396488e161745b8f66ed"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/yahyatoubali/televault/releases/download/v#{version}/televault-v#{version}-linux-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_X86_64_SHA256_AFTER_RELEASE"
+      sha256 "26d7d0f11593223e9d33de69caab1ffe82ef16b5e06d30c6c58fbbc823c8c3dc"
     end
     on_arm do
       url "https://github.com/yahyatoubali/televault/releases/download/v#{version}/televault-v#{version}-linux-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_AARCH64_SHA256_AFTER_RELEASE"
+      sha256 "9b58546f8188a7a543d57d147a8acde9bbf3de9f2338cd63045dcdfa49265e20"
     end
   end
 
